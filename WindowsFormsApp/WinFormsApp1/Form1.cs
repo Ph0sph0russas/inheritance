2767d5bfb3ebf434d16a64a852b12c73d50de10c
Ph0sph0russas <danila.ustiuzhanin@gmail.com>
2026-03-25 13:01:02 +0800

    Добавлен класс показа информации и начальная логика количества элементов

--- WindowsFormsApp/WinFormsApp1/Form1.cs
+++ WindowsFormsApp/WinFormsApp1/Form1.cs
@@ -2,6 +2,7 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        List<Weather> weatherList = new List<Weather>();
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +10,36 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            this.weatherList.Clear();
+            for (var i = 0; i<10; ++i)
+            {
+                this.weatherList.Add(new Sun());
+            }
+        }
+        private void ShowInfo()
+        {
+            int sunCount = 0;
+            int rainCount = 0;
+            int snowCount = 0;
 
+            foreach(var weather in this.weatherList)
+            {
+                if (weather is Sun)
+                {
+                    sunCount += 1;
+                }
+                else if (weather is Rain)
+                {
+                    rainCount += 1;
+                }
+                else if (weather is Snow)
+                {
+                    snowCount += 1;
+                }
+            }
+            txtInfo.Text = "Солнце\tДождь\tСнег";
+            txtInfo.Text += "\n";
+            txtInfo.Text += String.Format("{0}\t{1}\t{2}", sunCount, rainCount, snowCount);
         }
     }
 }
