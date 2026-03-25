4f2bf5fa529cc893135c334a2d833ddf9bd772cb
Ph0sph0russas <danila.ustiuzhanin@gmail.com>
2026-03-25 12:40:19 +0800

    Добавлен класс погоды и в нём созданы классы солнечной, снежной и дождевой погод

---
+++ WindowsFormsApp/WinFormsApp1/Weather.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// БУДТЕ ВНИМАТЕЛЬНЕЕ: ТУТ ДОЛЖЕН СТОЯТЬ ТОТ ЖЕ namespace что и в Program.cs
+namespace WinFormsApp1
+{
+    public class Sun
+    {
+        public int temperature=0;
+        public int altitudeAboveHorizon=0;
+        public bool presenceOfBreeze=false;
+    }
+
+    public class Rain
+    {
+        public int temperature = 0;
+        public int precipitationAmount = 0;
+        public bool rainbowPresence = false;
+        public bool stormPresence = false;
+    }
+
+    public enum snowType {small, flakes, moist};
+
+    public class Snow
+    {
+        public int temperature=0;
+        public snowType type = snowType.small;
+        public int snowdriftHeight = 0;
+    }
+}
